908bc81cb7ff89ab2a058bb529aa6638d180ff0e
walkerxian <cvchina@yahoo.com>
2024-03-16 12:22:59 +0800

    【Draw a triangle】重构主函数代码，分离到应用程序类soc_app_base和窗口函数soc_window类中

--- SocPipeline/src/main.cpp
+++ SocPipeline/src/main.cpp
@@ -1,35 +1,26 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
+#include "soc_app_base.hpp"
+
+//std
+
+#include <stdlib.h>
 #include <iostream>
+#include <stdexcept>
+
 
 int main() {
   
-  glfwInit();
+  soc::SocAppBase app{};
 
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+  try{
 
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    app.run();
 
-  std::cout << extensionCount << " extensions supported\n";
+  }catch(const std::exception &e){
 
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
+    std::cerr<<e.what() << '\n';
+    return EXIT_FAILURE;
+  } 
 
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-  }
-
-  glfwDestroyWindow(window);
-
-  glfwTerminate();
-
-  return 0;
+  return EXIT_SUCCESS; 
 }
