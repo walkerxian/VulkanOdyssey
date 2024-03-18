ccf315a44e3a67c5fdd20b3add8869340f3f82cd
walkerxian <cvchina@yahoo.com>
2024-03-18 16:57:06 +0800

    Revert "【Draw a triangle】add.bat window compile script, and fixed the .vert error"
    This reverts commit 5b2e23962b81098548ade8dee6643b2ca815f0d6.

--- SocPipeline/src/compile.bat
+++
@@ -1,3 +0,0 @@
-"D:\Program Files\VulkanSDK\1.3.275.0\Bin\glslc.exe"  shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
-"D:\Program Files\VulkanSDK\1.3.275.0\Bin\glslc.exe"  shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
-pause
